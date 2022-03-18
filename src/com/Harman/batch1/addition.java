4a20a774486d37f88613fbfba2f2f2d509aec549
Dasiripalli Joshna <91077542+DasiripalliJoshna@users.noreply.github.com>
2022-03-18 12:25:19 +0530

    Delete src/com directory

--- src/com/Harman/batch1/addition.java
+++
@@ -1,9 +0,0 @@
-package com.Harman.batch1;
-
-public class addition {
-    public int addition2(int a, int b)
-    {
-        int c=a+b;
-        return c;
-    }
-}
--- src/com/Harman/batch1/subtraction.java
+++
@@ -1,8 +0,0 @@
-package com.Harman.batch1;
-
-public class subtraction {
-    public int subtract(int a, int b) {
-        int c = a - b;
-    return c;
-    }
-}
