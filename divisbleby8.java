88463e8e0f766382be3a1a25f1ebd91b39423173
DasiripalliJoshna <joshnachandra8@gmail.com>
2022-03-18 11:27:51 +0530

    Git_versioning

---
+++ divisbleby8.java
@@ -0,0 +1,17 @@
+public class divisbleby8 {
+
+        public static void main(String[] args) {
+
+            int a=26;
+            if(a%8==0)
+            {
+                System.out.println(" divisible by 8 ");
+
+            }
+            else
+            {
+                System.out.println("not divisble by 8");
+            }
+        }
+    }
+
